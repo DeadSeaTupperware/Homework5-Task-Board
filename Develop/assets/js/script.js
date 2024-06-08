48e2708fb84fa826ed2e4fc766607a8de246cba8
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-07 20:34:31 -0400

    Add task modal configured

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const addTaskButton = document.querySelector("#add-task-btn");
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -19,6 +20,9 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    event.preventDefault();
+
+    $('#myModal').modal(options);
 
 }
 
@@ -35,4 +39,9 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
+});
+
+// Event Listener for add task.
+addTaskButton.addEventListener('click', function (event) {
+handleAddTask();
 });
