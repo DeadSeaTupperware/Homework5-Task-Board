2a9d5be871d7469943a045830d2fcee77a10c501
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-09 15:17:21 -0400

    added droppability function

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -21,16 +21,16 @@ function createTaskCard(task) {
       </div>
     `);
 
-    if (task.deadline && task.status !== 'done') {
-        const now = dayjs();
-        const taskDueDate = dayjs(task.deadline, 'MM-DD-YYYY');
-        if (now.isSame(taskDueDate, 'day')) {
-          taskCard.addClass('bg-warning text-white');
-        } else if (now.isAfter(taskDueDate)) {
-          taskCard.find('.card-body').addClass('bg-danger text-white');
-          taskCard.find('.delete-btn').addClass('border-light');
-        }
-      }
+    // if (task.deadline && task.status !== 'done') {
+    //     const now = dayjs();
+    //     const taskDueDate = dayjs(task.deadline, 'MM-DD-YYYY');
+    //     if (now.isSame(taskDueDate, 'day')) {
+    //       taskCard.addClass('bg-warning text-white');
+    //     } else if (now.isAfter(taskDueDate)) {
+    //       taskCard.find('.card-body').addClass('bg-danger text-white');
+    //       taskCard.find('.delete-btn').addClass('border-light');
+    //     }
+    //   }
 
      return taskCard;
 
@@ -93,7 +93,14 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+    const taskId = ui.draggable.data("id");
+    const newStatus = $(this).attr("id");
+  
+    const taskIndex = taskList.findIndex(task => task.id === taskId);
+    taskList[taskIndex].status = newStatus;
+  
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
